a92b24ac3465dd408d4a441274c9fd6b7cbd2df8
DzmitryBasiachenka <bsdim333@gmail.com>
2021-03-23 10:54:16 +0300

    Lesson 44. Context API. Added StoreContext file

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import { addPostActionCreator, updateNewPostActionCreator } from '../../../redux/profile-reducer';
+import StoreContext from '../../../StoreContext';
 import MyPosts from './MyPosts';
 
-const MyPostsContainer = (props) => {
-  let state = props.store.getState();
+const MyPostsContainer = () => {
+  return (
+    <StoreContext.Consumer>
+      {
+        (store) => {
+          let state = store.getState();
 
-  let addPost = () => {
-    props.store.dispatch(addPostActionCreator());
-  }
+          let addPost = () => {
+            store.dispatch(addPostActionCreator());
+          }
 
-  let onPostChange = (text) => {
-    let action = updateNewPostActionCreator(text);
-    props.store.dispatch(action);
-  }
+          let onPostChange = (text) => {
+            let action = updateNewPostActionCreator(text);
+            store.dispatch(action);
+          }
 
-  return (<MyPosts updateNewPostText={onPostChange} 
-                   addPost={addPost} 
-                   posts={state.profilePage.posts}
-                   newPostText={state.profilePage.newPostText} />)
+          return <MyPosts updateNewPostText={onPostChange}
+            addPost={addPost}
+            posts={state.profilePage.posts}
+            newPostText={state.profilePage.newPostText} />
+        }
+      }
+    </StoreContext.Consumer>)
 }
 
 export default MyPostsContainer;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './redux/store';
+import { BrowserRouter } from 'react-router-dom';
+import StoreContext, { Provider } from './StoreContext';
 
 export let renderEntireTree = (state) => {
     ReactDOM.render(
-        <React.StrictMode>
-            <App state={state} dispatch={store.dispatch.bind(store)} store={store} />
-        </React.StrictMode>,
+        <BrowserRouter>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </BrowserRouter>,
         document.getElementById('root')
     );
 }
