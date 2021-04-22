5e7cb7134a67f53771b968eae8db41474cf54d39
DzmitryBasiachenka <bsdim333@gmail.com>
2021-04-22 16:08:28 +0300

    Lesson 61. Dispaying user info signed up in social-network; auth/me

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import './Header.css';
+import { NavLink } from 'react-router-dom';
+import style from './Header.module.css';
 
-const Header = () => {
-    return <header className='header'>
+const Header = (props) => {
+    return <header className={style.header}>
         <img src='https://cdn.scores24.ru/upload/team/w90-h90/975/75a/a6ec0b18d947221764d8721aa9095d8673.png' />
+
+        <div className={style.loginBlock}>
+            { props.isAuth ? props.login : <NavLink to='/login'>Login</NavLink> }
+        </div>
     </header>
 }
 
